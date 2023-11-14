a2db724768bc0b60655ee3d2d8fb1c4468a157c5
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-11-14 13:40:15 +0200

    refactor

--- specs/messagefactory.spec.mjs
+++ specs/messagefactory.spec.mjs
@@ -1,9 +1,12 @@
-import { MessageFactory } from '../lib/registry.mjs';
+import { Message, MessageFactory } from '../lib/registry.mjs';
 const suite = describe('when receiving unicode characters over http given an acceptable total character length', () => {
     it('should utilize the message factory and create a message', () => {
         const message = MessageFactory.create('Hello World');
         expect(message).toBeDefined();
         expect(message).not.toBeNull();
+        expect(message).toBeInstanceOf(Message);
+        expect(message.Id).toBeDefined();
+        expect(message.text).toBe('Hello World');
     });
 });
 process.specs.set(suite, []);
