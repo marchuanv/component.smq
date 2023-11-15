99343a0bed00a4ba823ef0504e9223c84834ef52
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-11-16 01:44:07 +0200

    refactor

--- specs/queue.spec.mjs
+++ specs/queue.spec.mjs
@@ -1,15 +1,18 @@
 import { Connection, ConnectionOptions, MessageFactory, MessagePriority, Queue } from '../lib/registry.mjs';
 const suite = fdescribe('when ', () => {
-    it('should ', async () => {
+    it('should ', (done) => {
         const connectionOptions = new ConnectionOptions(3, 10000, 'localhost', 8080, 'localhost', 8080);
         const connection = new Connection(connectionOptions);
         const queue = new Queue(connection);
         const newMessage = MessageFactory.create('Hello World', MessagePriority.High);
-        await queue.enqueue(newMessage);
-        const message = await queue.dequeue();
-        expect(Number(message.headers.priority)).toBe(newMessage.headers.priority);
-        expect(message.body.Id).toBe(newMessage.body.Id);
-        expect(message.body.data).toBe(newMessage.body.data);
+        queue.enqueue(newMessage);
+        setTimeout(() => {
+            const message = queue.dequeue();
+            expect(Number(message.headers.priority)).toBe(newMessage.headers.priority);
+            expect(message.body.Id).toBe(newMessage.body.Id);
+            expect(message.body.data).toBe(newMessage.body.data);
+            done();
+        }, 1000);
     });
 });
 process.specs.set(suite, []);
