1c4319f622d7652a3040fbc244db995e0192ff68
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-11-15 17:57:14 +0200

    refactor

---
+++ specs/queue.spec.mjs
@@ -0,0 +1,13 @@
+import { Connection, ConnectionOptions, MessageFactory, Queue } from '../lib/registry.mjs';
+const suite = fdescribe('when ', () => {
+    it('should ', async () => {
+        const connectionOptions = new ConnectionOptions(3, 10000, 'localhost', 8080, 'localhost', 8080);
+        const connection = new Connection(connectionOptions);
+        await connection.open();
+        const queue = new Queue(connection);
+        const message = MessageFactory.create('Hello World');
+        const responseMessage = await queue.enqueue(message);
+        expect(JSON.stringify(responseMessage)).toBe(JSON.stringify({ Id: message.body.Id, data: 'message received and is valid' }));
+    });
+});
+process.specs.set(suite, []);
