0cf736e5fde1ac00276dc7e259d3608007a002fe
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-11-16 08:02:58 +0200

    refactor

--- specs/queue.spec.mjs
+++ specs/queue.spec.mjs
@@ -9,6 +9,7 @@ const suite = fdescribe('when ', () => {
         setTimeout(() => {
             const message = queue.dequeue();
             expect(Number(message.headers.priority)).toBe(newMessage.headers.priority);
+            expect(Number(message.headers.time)).toBe(newMessage.headers.time);
             expect(message.body.Id).toBe(newMessage.body.Id);
             expect(message.body.data).toBe(newMessage.body.data);
             done();
